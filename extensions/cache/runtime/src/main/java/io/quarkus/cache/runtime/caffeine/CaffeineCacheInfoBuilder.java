a59f75d64233349a0f8efa0d3e49e3d6f54c7a57
Gwenneg Lepage <glepage@redhat.com>
2023-01-10 23:41:06 +0100

    Allow global default cache configuration

--- extensions/cache/runtime/src/main/java/io/quarkus/cache/runtime/caffeine/CaffeineCacheInfoBuilder.java
+++ extensions/cache/runtime/src/main/java/io/quarkus/cache/runtime/caffeine/CaffeineCacheInfoBuilder.java
@@ -1,11 +1,13 @@
 package io.quarkus.cache.runtime.caffeine;
 
+import static io.quarkus.cache.runtime.CacheConfig.CaffeineConfig.CaffeineCacheConfig;
+
 import java.util.Collections;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import io.quarkus.cache.runtime.CacheConfig;
-import io.quarkus.cache.runtime.CacheConfig.CaffeineConfig.CaffeineNamespaceConfig;
 
 public class CaffeineCacheInfoBuilder {
 
@@ -13,18 +15,49 @@ public class CaffeineCacheInfoBuilder {
         if (cacheNames.isEmpty()) {
             return Collections.emptySet();
         } else {
-            return cacheNames.stream().map(cacheName -> {
-                CaffeineCacheInfo cacheInfo = new CaffeineCacheInfo();
-                cacheInfo.name = cacheName;
-                CaffeineNamespaceConfig namespaceConfig = cacheConfig.caffeine.namespace.get(cacheInfo.name);
-                if (namespaceConfig != null) {
-                    namespaceConfig.initialCapacity.ifPresent(capacity -> cacheInfo.initialCapacity = capacity);
-                    namespaceConfig.maximumSize.ifPresent(size -> cacheInfo.maximumSize = size);
-                    namespaceConfig.expireAfterWrite.ifPresent(delay -> cacheInfo.expireAfterWrite = delay);
-                    namespaceConfig.expireAfterAccess.ifPresent(delay -> cacheInfo.expireAfterAccess = delay);
-                    cacheInfo.metricsEnabled = namespaceConfig.metricsEnabled;
+            CaffeineCacheConfig defaultConfig = cacheConfig.caffeine.defaultConfig;
+
+            return cacheNames.stream().map(new Function<String, CaffeineCacheInfo>() {
+                @Override
+                public CaffeineCacheInfo apply(String cacheName) {
+
+                    CaffeineCacheInfo cacheInfo = new CaffeineCacheInfo();
+                    cacheInfo.name = cacheName;
+
+                    CaffeineCacheConfig namedCacheConfig = cacheConfig.caffeine.cachesConfig.get(cacheInfo.name);
+
+                    if (namedCacheConfig != null && namedCacheConfig.initialCapacity.isPresent()) {
+                        cacheInfo.initialCapacity = namedCacheConfig.initialCapacity.getAsInt();
+                    } else if (defaultConfig.initialCapacity.isPresent()) {
+                        cacheInfo.initialCapacity = defaultConfig.initialCapacity.getAsInt();
+                    }
+
+                    if (namedCacheConfig != null && namedCacheConfig.maximumSize.isPresent()) {
+                        cacheInfo.maximumSize = namedCacheConfig.maximumSize.getAsLong();
+                    } else if (defaultConfig.maximumSize.isPresent()) {
+                        cacheInfo.maximumSize = defaultConfig.maximumSize.getAsLong();
+                    }
+
+                    if (namedCacheConfig != null && namedCacheConfig.expireAfterWrite.isPresent()) {
+                        cacheInfo.expireAfterWrite = namedCacheConfig.expireAfterWrite.get();
+                    } else if (defaultConfig.expireAfterWrite.isPresent()) {
+                        cacheInfo.expireAfterWrite = defaultConfig.expireAfterWrite.get();
+                    }
+
+                    if (namedCacheConfig != null && namedCacheConfig.expireAfterAccess.isPresent()) {
+                        cacheInfo.expireAfterAccess = namedCacheConfig.expireAfterAccess.get();
+                    } else if (defaultConfig.expireAfterAccess.isPresent()) {
+                        cacheInfo.expireAfterAccess = defaultConfig.expireAfterAccess.get();
+                    }
+
+                    if (namedCacheConfig != null && namedCacheConfig.metricsEnabled.isPresent()) {
+                        cacheInfo.metricsEnabled = namedCacheConfig.metricsEnabled.get();
+                    } else if (defaultConfig.metricsEnabled.isPresent()) {
+                        cacheInfo.metricsEnabled = defaultConfig.metricsEnabled.get();
+                    }
+
+                    return cacheInfo;
                 }
-                return cacheInfo;
             }).collect(Collectors.toSet());
         }
     }
